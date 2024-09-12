fb39207acba246c7701f933fef46515fa0fdb976
Williams Osimen <greycodes@protonmail.com>
2024-09-11 21:48:41 -0700

    all worls

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 for num in range(0, 100):
-    print(f"{num:02}", end=", ")
+    if num == 99:
+        print("{}".format(num))
+    else:
+        print("{:02}".format(num), end=",")
