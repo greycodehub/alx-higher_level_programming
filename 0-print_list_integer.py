296e88facd7499e66450b915a39469ddc857d592
Williams Osimen <greycodes@protonmail.com>
2024-09-16 21:28:02 -0700

    corrected the code error

--- 0-print_list_integer.py
+++ 0-print_list_integer.py
@@ -2,5 +2,5 @@
 
 def print_list_integer(my_list=[]):
     """prints all the integers of a list"""
-    for l in range(len(my_list):
+    for l in range(len(my_list)):
             print("{:d}".format(my_list[l]))
