6eafa85cfe140ba678b7db9ce0de6fa91cc85ff5
Williams Osimen <greycodes@protonmail.com>
2025-04-10 13:04:12 +0000

    instance of a class square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 1-square.py
+
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): defines the size of the new square.
+        """
+        self.__size = size
