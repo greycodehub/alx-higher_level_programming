9cc9d675057d12406520b6d253f20b1a31201bf8
Williams Osimen <greycodes@protonmail.com>
2025-04-10 01:18:12 +0000

    roman ways~

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # 12-roman_to_int.py
 
-
 def roman_to_int(roman_string):
     """Convts a roman numeral to an integer."""
     if (not isinstance(roman_string, str) or
@@ -29,4 +28,4 @@ def roman_to_int(roman_string):
 
         else:
             num += roman_dict[roman_string[i]]
-    return (num)i
+    return (num)
